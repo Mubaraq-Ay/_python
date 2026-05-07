07953e4df8f1f8c3634a711f58a92f98a1857dfc
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-05-06 19:55:11 -0700

    initial commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,47 @@
+i = 3
+j = 4
+
+print( i + j)
+print(i - j)
+print(i * j)
+print(i % j)
+print(i / j)
+print(i ** j)
+print(i // j)
+
+name = 'mubaraq'
+familyName = 'ayanleke'
+country = 'qatar'
+
+print(name)
+print(familyName)
+print(country)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(name))
+print(type(familyName))
+print(type(country))
+
+'''
+    int = 3
+    float = 3.1
+    complex = 3 - 4j
+    str = 'my name is mubaraq'
+    boolean = true / false   in = True
+    list = ['a', 'b', 'c']
+    tuple = ('a', 'b', 'c')
+    set = {1,2,3}
+    dict = {
+        "name": "mansd",
+        "age": 18
+    }
+'''
+
+# euclidean distance
+
+d = ((10 - 2)**2 + (8 - 3)**2) ** 0.5
+print(d)
