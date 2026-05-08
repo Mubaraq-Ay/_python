e36539c3125dcf4a0fd9a111e9e93196f8e5c618
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-05-07 18:43:38 -0700

    variable

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,15 +1,45 @@
 # day 2: days of python programming
-#exercises
+# exercises
 
-# first_name = 'mubaraq'
-# last_name = 'ayanleke'
-# full_name = 'mubaraq ayanleke'
-# country = 'nigeria'
-# city = 'lagos'
-# age = 18
-# year = 2026
-# is_married = False
-# is_true = False
-# is_light_on = True
+first_name = 'mubaraq'
+last_name = 'ayanleke'
+full_name = 'mubaraq ayanleke'
+country = 'nigeria'
+city = 'lagos'
+age = 18
+year = 2026
+is_married = False
+is_true = False
+is_light_on = True
+
+# print('first name: ', type(first_name), 'last_name: ', type(last_name), 'full_name: ', type(full_name), 'country:', type(country), 'city', type(city), 'age', type(age), 'year', type(year), 'is_married', type(is_married), 'is_true', type(is_true), 'is_light_on', type(is_light_on))
 
 # first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light_on = 'mubaraq', 'ayanleke', 'ayanleke mubaraq', 'nigeria', 'lagos', 18, True, True, False
+
+
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+print(total)
+
+diff = num_one - num_two
+print(diff)
+
+product = num_one * num_two
+print(product)
+
+division = num_one / num_two
+print(division)
+
+remainder = num_one % num_two
+print(remainder)
+
+exp = num_one ** num_two
+print(exp)
+
+floor_division = num_one // num_two
+print(floor_division)
