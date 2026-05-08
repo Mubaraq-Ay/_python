20804deba02917305bd4480e1b616c2a70b31fab
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-05-07 18:05:40 -0700

    ex 1

---
+++ day_2/variables.py
@@ -0,0 +1,15 @@
+# day 2: days of python programming
+#exercises
+
+# first_name = 'mubaraq'
+# last_name = 'ayanleke'
+# full_name = 'mubaraq ayanleke'
+# country = 'nigeria'
+# city = 'lagos'
+# age = 18
+# year = 2026
+# is_married = False
+# is_true = False
+# is_light_on = True
+
+# first_name, last_name, full_name, country, city, age, year, is_married, is_true, is_light_on = 'mubaraq', 'ayanleke', 'ayanleke mubaraq', 'nigeria', 'lagos', 18, True, True, False
