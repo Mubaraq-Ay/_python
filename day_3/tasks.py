1b2c66ed2fb1d76b5f7cde006b7ea6ff91367d41
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-05-09 18:42:02 -0700

    day 3, tasks radius of a circle

---
+++ day_3/tasks.py
@@ -0,0 +1,37 @@
+age = 18
+my_height = 189.6
+complex_num = 3 + 3j
+
+
+# a script to solve the area of a triangle
+
+# base = (float(input('enter the base: ')))
+# height = (float(input('enter the height: ')))
+
+# area_of_triangle = 1/2 * base * height
+
+# print('The area of the triangle is ', area_of_triangle)
+
+# a script that calculates the perimeter of a triangle
+ 
+# a = float(input('enter 1st side: '))
+# b = float(input('enter 2nd side: '))
+# c = float(input('enter 3rd side: '))
+
+# perimeter_of_triangle = a + b + c
+
+# print('the perimeter of the triangle is: ', perimeter_of_triangle)
+
+
+# length and width of a rectangle
+
+length = float(input('enter the length: '))
+width = float(input('enter the width: '))
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print('area of a rectangle: ', area)
+print('perimeter of a rectangle: ', perimeter)
+
+# radius of a circle 
