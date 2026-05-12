8fc01730ddfec2ed168c63da15f604ca17bd50d7
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-05-11 19:49:56 -0700

    multiline string

--- day_4/index.py
+++ day_4/index.py
@@ -11,3 +11,11 @@ print(len(greeting))
 sentence = 'i hope you are enjoying 30 days of python challenge'
 print(sentence)
 print(len(sentence))
+
+# multiline string
+
+multiline_string = '''my name is mubaraq and i enjoy coding and solving logical tasks'''
+print(multiline_string)
+
+multiline_string = """my name is mubaraq and i enjoy coding and solving logical problems."""
+print(multiline_string)
