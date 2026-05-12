768d186396f5f6b892ac2b47db8d868ed332afd1
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-05-11 19:52:39 -0700

    concatenation

--- day_4/index.py
+++ day_4/index.py
@@ -1,21 +1,30 @@
 # strings. 
 
-letter = 'p'
-print(letter)
-print(len(letter))
+# letter = 'p'
+# print(letter)
+# print(len(letter))
 
-greeting = 'hello, world!'
-print(greeting)
-print(len(greeting))
+# greeting = 'hello, world!'
+# print(greeting)
+# print(len(greeting))
 
-sentence = 'i hope you are enjoying 30 days of python challenge'
-print(sentence)
-print(len(sentence))
+# sentence = 'i hope you are enjoying 30 days of python challenge'
+# print(sentence)
+# print(len(sentence))
 
-# multiline string
+# # multiline string
 
-multiline_string = '''my name is mubaraq and i enjoy coding and solving logical tasks'''
-print(multiline_string)
+# multiline_string = '''my name is mubaraq and i enjoy coding and solving logical tasks'''
+# print(multiline_string)
 
-multiline_string = """my name is mubaraq and i enjoy coding and solving logical problems."""
-print(multiline_string)
+# multiline_string = """my name is mubaraq and i enjoy coding and solving logical problems."""
+# print(multiline_string)
+
+# # concatenation
+
+first_name = 'mubaraq'
+last_name = 'ayanleke'
+space = ' '
+
+full_name = first_name + space + last_name
+print(full_name)
