d4913ce34c467e31488b69ad5a976e228937065d
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-05-12 00:10:10 -0700

    escape sequences in string

--- day_4/index.py
+++ day_4/index.py
@@ -22,16 +22,35 @@
 
 # # concatenation
 
-first_name = 'mubaraq'
-last_name = 'ayanleke'
-space = ' '
+# first_name = 'mubaraq'
+# last_name = 'ayanleke'
+# space = ' '
 
-full_name = first_name + space + last_name
-print(full_name)
+# full_name = first_name + space + last_name
+# print(full_name)
 
-# checking the length of a string using len() built in function
+# # checking the length of a string using len() built in function
 
-print(len(first_name))
-print(len(last_name))
-print(len(full_name))
-print(len(first_name) > len(last_name))
+# print(len(first_name))
+# print(len(last_name))
+# print(len(full_name))
+# print(len(first_name) > len(last_name))
+
+# escape sequences in strings
+
+''' \n - new line
+    \t - Tab means(8 spaces)
+    \\ - back slash
+    \' - single quote
+    \" - double quote
+'''
+
+# print('i hope everyone is enjoying the python challenge. \nAre you?')
+print('Days\tTopics\tExercises') # adding tab space or 4 spaces.
+print('Day 1\t5\t5')
+print('Day 2\t6\t20')
+print('Day 3\t5\t23')
+print('Day 4\t1\t35')
+
+print('this is a backlash symbol (\\)')
+print('in every programming language it starts with \"hello, world!\"')
