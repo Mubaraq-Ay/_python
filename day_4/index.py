abbab9eca397cb3a5fb954a472f22e68fb32fea5
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-05-11 19:44:55 -0700

    creating a string

---
+++ day_4/index.py
@@ -0,0 +1,13 @@
+# strings. 
+
+letter = 'p'
+print(letter)
+print(len(letter))
+
+greeting = 'hello, world!'
+print(greeting)
+print(len(greeting))
+
+sentence = 'i hope you are enjoying 30 days of python challenge'
+print(sentence)
+print(len(sentence))
