5f1586a5f274877cb3d437670e822d54a1876c3b
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-05-16 14:44:46 -0700

    bio generator

---
+++ projects/bio_generator.py
@@ -0,0 +1,7 @@
+name = input('Enter your name: ')
+age = int(input('How old are you? '))
+country = input('which country are you from? ')
+height = float(input('enter your height: '))
+
+bio = f'i am {name}, a computer science student from {country}, i am {age} years old, and my height is {height}.'
+print(bio)
