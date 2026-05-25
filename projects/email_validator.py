4b6ceae56a3be356ae5393d07895bc23cbd7cb38
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-05-25 12:33:36 -0700

    email validator

--- projects/email_validator.py
+++ projects/email_validator.py
@@ -1,2 +1,13 @@
 user_email = input('Enter your email: ')
+contain_at = '@' in user_email
+contain_dot = '.' in user_email
 
+at_index = user_email.find('@')
+user_name = user_email[:at_index]
+
+domain = user_email[at_index + 1:]
+is_email_valid = contain_at and contain_dot
+
+print(f'Username: {user_name}')
+print(f'Domain: {domain}')
+print(f'Valid email: {is_email_valid}')
