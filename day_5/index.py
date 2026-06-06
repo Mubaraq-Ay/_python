419f26d5d68abf597cc889419b5f083af96d9fd3
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-06 12:49:12 -0700

    joining using extend()

--- day_5/index.py
+++ day_5/index.py
@@ -346,3 +346,26 @@ vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
 
 fruits_and_vegetables = fruits + vegetables
 print(fruits_and_vegetables)
+
+# joining, using extend()
+
+num1 = [0, 1, 2, 3]
+num2 = [4, 5, 6]
+
+num1.extend(num2)
+print('Numbers:', num1)
+
+negative_numbers = [-5, -4, -3, -2, -1]
+positive_numbers = [1, 2, 3, 4, 5]
+zero = [0]
+
+negative_numbers.extend(zero)
+negative_numbers.extend(positive_numbers)
+
+print('Integers:', negative_numbers)
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+
+fruits.extend(vegetables)
+print('fruits and vegetables:', fruits)
