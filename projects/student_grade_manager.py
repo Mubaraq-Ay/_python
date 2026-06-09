330e8642909b66af01715e6d819910241676acdb
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-09 00:44:32 -0700

    student grade manager

---
+++ projects/student_grade_manager.py
@@ -0,0 +1,8 @@
+students_names = ['mubaraq', 'baseet', 'kunle', 'wale', 'mbappe']
+scores = ['79', '82', '77', '80', '88']
+
+print(f'{students_names[0]}: {scores[0]}')
+print(f'{students_names[1]}: {scores[1]}')
+print(f'{students_names[2]}: {scores[2]}')
+print(f'{students_names[3]}: {scores[3]}')
+print(f'{students_names[4]}: {scores[4]}')
