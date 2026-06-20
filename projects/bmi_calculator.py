98537f169ea20c4b44d204d50af34f508b69df77
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-20 16:51:30 -0700

    folder rename

--- projects/bmi_calculator.py
+++
@@ -1,7 +0,0 @@
-weight = float(input('enter your weight in kg: '))
-height = float(input('enter your height in m: '))
-
-bmi = weight / height**2
-print(f'=========== Your BMI =========')
-print(f'Your BMI: {bmi:.2f}')
-print(f'======== stay healthy :) ========')
