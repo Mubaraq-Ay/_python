a24a6d4c5b6199b3f44cb84e3f3f53364ea04367
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-23 15:36:55 -0700

    task 2

--- day_6/task.py
+++ day_6/task.py
@@ -21,5 +21,22 @@ print(family_members)
 
 # level 2
 
+# unpack siblings and parents
 
+*siblings, father, mother = family_members
+
+parents = (father, mother)
+print(siblings)
+print(parents)
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+vegetables = ('Tomato', 'Potato', 'Cabbage','Onion', 'Carrot')
+animal_products = ('milk', 'egg', 'ponmo', 'meat', 'fish')
+
+food_stuff_tp = fruits + vegetables + animal_products
+print(food_stuff_tp)
+
+food_stuff_lt = list(food_stuff_tp)
+print(food_stuff_lt)
 
