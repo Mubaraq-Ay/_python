df23b8ee1454ab7b5172c28c81737fd012b2e91f
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-22 17:00:29 -0700

    system provision

---
+++ beginner_projects/system_provision.py
@@ -0,0 +1,15 @@
+system_tools = [
+    'git', 'python', 'linux'
+]
+
+frontend = input('enter your preferred frontend framework: ')
+backend = input('enter your preferred backend framework: ')
+
+system_tools.append(frontend)
+system_tools.append(backend)
+
+print(system_tools)
+print('Number of tools: ',len(system_tools))
+
+command = 'apt-get install ' + ' '.join(system_tools)
+print(command) 
