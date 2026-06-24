378e509d131ba0c17982986e8448bf30d5cc32e5
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-24 06:48:29 -0700

    intersection

--- day_7/index.py
+++ day_7/index.py
@@ -80,3 +80,16 @@ print(fruits.union(vegetables))
 print(fruits | vegetables)
 
 # finding intersection items
+# Intersection returns a set of items which are in both the sets or using & symbol. 
+
+whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+even_numbers = {0, 2, 4, 6, 8, 10}
+
+intercept = whole_numbers.intersection(even_numbers)
+
+print(intercept)
+
+python = {'p', 'y', 't', 'h', 'o','n'}
+dragon = {'d', 'r', 'a', 'g', 'o','n'}
+
+print(python.intersection(dragon))
