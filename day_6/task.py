d3e169baa529a248a6de6dcf73925b66c10e7f4d
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-23 21:00:56 -0700

    tuplws

--- day_6/task.py
+++ day_6/task.py
@@ -40,3 +40,31 @@ print(food_stuff_tp)
 food_stuff_lt = list(food_stuff_tp)
 print(food_stuff_lt)
 
+# find the middle item
+
+middle_index = len(food_stuff_tp) // 2
+print(middle_index)
+
+middle_items = food_stuff_tp[middle_index - 1 : middle_index + 1]
+print(middle_items)
+
+# slice out the first 3 items
+
+first_three_items = food_stuff_lt[0:3]
+print(first_three_items)
+
+last_three_items = food_stuff_lt[-3:]
+print(last_three_items)
+
+# delete food tuple
+
+del food_stuff_tp
+# print(food_stuff_tp)
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+
+check_estonia = 'Estonia' in nordic_countries
+print(check_estonia)
+
+check_iceland = 'Iceland' in nordic_countries
+print(check_iceland)
