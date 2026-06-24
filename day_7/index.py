bb085fbc3210954a4f3769b6567958592648f4dc
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-24 06:30:00 -0700

    clearing items in a set

--- day_7/index.py
+++ day_7/index.py
@@ -11,3 +11,40 @@ print(len(fruits))
 # accessing items in a set
 fruits = {'banana', 'orange', 'mango', 'lemon'}
 print('does mango exist in fruits?', 'mango' in fruits)
+
+# adding items to a set, we use add()
+
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+fruits.add('lime')
+
+print(fruits)
+
+# we add multiple items using update()
+
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+vegetables = ('tomato', 'potato', 'cabbage','onion', 'carrot')
+
+fruits.update(vegetables)
+print(fruits)
+
+# removing items from set.
+# we can use remove(), discard(), and pop(), but pop() removes a random item
+
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+fruits.remove('banana')
+
+print(fruits)
+fruits.discard('mango')
+
+print(fruits)
+
+fruits.pop()
+print(fruits)
+
+# to know the removed item
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+removed_item = fruits.pop() 
+
+print(removed_item)
+
+# clearing items in a set
