5cdb3ebd6ddc607ea301a01f3bc1a6df37b08ac6
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-24 15:51:42 -0700

    difference_update()

--- day_7/index.py
+++ day_7/index.py
@@ -139,9 +139,10 @@ print(dragon.difference(python))
 A = {1, 2, 3, 4, 5, 7}
 B = {3, 4, 6, 7, 8}
 
-# Removes 3 and 4 from A
 A.difference_update(B)
 
 print(A)   
-print(B)  # Output: {3, 4, 6} (B remains unchanged)
+print(B)  
+
+
 
