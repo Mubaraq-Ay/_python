ba97f36a5559659aac745c7c7f638faa09d9ae56
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-24 14:32:25 -0700

    day_7: superset and subset

--- day_7/index.py
+++ day_7/index.py
@@ -116,3 +116,12 @@ print(python.issuperset(dragon))
 print(python.issubset(dragon))
 
 print(dragon.issubset(python))
+
+
+# checking the difference between two sets
+# we use the minus symbol (-)
+
+whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+even_numbers = {0, 2, 4, 6, 8, 10}
+
+print(whole_numbers.difference(even_numbers))
