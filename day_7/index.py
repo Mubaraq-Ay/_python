e55384688752a5dcfae407d46a3e93936462e06f
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-24 15:41:53 -0700

    day_7: difference between 2 sets

--- day_7/index.py
+++ day_7/index.py
@@ -119,9 +119,17 @@ print(dragon.issubset(python))
 
 
 # checking the difference between two sets
-# we use the minus symbol (-)
+# we can use the minus symbol (-)
 
 whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 even_numbers = {0, 2, 4, 6, 8, 10}
 
 print(whole_numbers.difference(even_numbers))
+
+python = {'p', 'y', 't', 'o','n'}
+dragon = {'d', 'r', 'a', 'g', 'o','n'}
+
+print(python.difference(dragon))
+print(dragon.difference(python))
+
+# print(dragon - python)
