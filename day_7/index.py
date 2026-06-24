bd8999de9b8c51db03c83a148a9f5b5c522db516
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-24 06:14:58 -0700

    sets

--- day_7/index.py
+++ day_7/index.py
@@ -1,1 +1,13 @@
 # sets
+
+# set is a collection of unordered and un-indexed distinct elements
+
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+
+# finding length of a set
+
+print(len(fruits))
+
+# accessing items in a set
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+print('does mango exist in fruits?', 'mango' in fruits)
