1c5f8bbe079e4091670216fff60fd23edf0acdad
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-27 02:16:43 -0700

    accessing items

--- day_8/index.py
+++ day_8/index.py
@@ -19,6 +19,25 @@ person = {
 
 print(len(person))
 
+# accessing dictionary items
+
+# we can access dictionary items by referring its key name
+
+print(person['first_name'])
+print(person['age'])
+print(person['skills'][0])
+print(person['address']['street'])
+# print(person['city'])
+
+# Accessing an item by key name raises an error if the key does not exist. 
+# To avoid this error first we have to check if a key exist or we can use the get method. 
+# The get method returns None, which is a NoneType object data type, if the key does not exist.
+
+print(person.get('first_name'))
+print(person.get('skills'))
+print(person.get('skills')[0])
+print(person.get('city')) # returns none
+
 
 
 
