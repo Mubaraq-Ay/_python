8d35efe1b90160464500a39e573d59f0a28c81e1
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-27 01:49:24 -0700

    dictionary length

--- day_8/index.py
+++ day_8/index.py
@@ -1,5 +1,24 @@
 # dictionary
 
+# a dictionary is a collection of unordered, modifiable(mutable) paired (key:value) data type.
+
+person = {
+    'first_name': 'mubaraq',
+    'last_name': 'ayanleke',
+    'age': 18,
+    'country': 'nigeria',
+    'is_married': True,
+    'skills': ['python', 'linux', 'mongodb', 'node', 'aws'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+}
+
+# dictionary length
+
+print(len(person))
+
 
 
 
