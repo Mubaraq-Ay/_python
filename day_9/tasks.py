95d516ea20f5d53c6488d670bfcb904a595f7c32
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-06-28 21:33:52 -0700

    conditionals: tasks

--- day_9/tasks.py
+++ day_9/tasks.py
@@ -1,91 +1,91 @@
 # exercises
 
-# user_details = int(input('enter your age: '))
-# years = 18 - user_details
+user_details = int(input('enter your age: '))
+years = 18 - user_details
 
-# if user_details >= 18:
-#     print('you are old enough to drive.')
-# elif user_details < 18:
-#     print(f'ypu need {years} more years to drive.')
-# else:
-#     print('invalid age entered')
+if user_details >= 18:
+    print('you are old enough to drive.')
+elif user_details < 18:
+    print(f'ypu need {years} more years to drive.')
+else:
+    print('invalid age entered')
 
    
 
-# my_age = 18
-# your_age = int(input('enter your age: '))
-# age_gap = your_age - my_age
+my_age = 18
+your_age = int(input('enter your age: '))
+age_gap = your_age - my_age
 
-# if your_age > my_age:
-#     print(f'you are {age_gap} years older than me')
-# elif my_age == your_age:
-#     print('we are age mates g.')
-# else:
-#     print(f'you are {abs(age_gap)} years younger than me') 
+if your_age > my_age:
+    print(f'you are {age_gap} years older than me')
+elif my_age == your_age:
+    print('we are age mates g.')
+else:
+    print(f'you are {abs(age_gap)} years younger than me') 
 
 
 # 3
 
-# a = int(input('enter a number: '))
-# b = int(input('enter a number: '))
+a = int(input('enter a number: '))
+b = int(input('enter a number: '))
 
-# if a > b:
-#     print('a is greater than b')
-# elif a < b:
-#     print('a is smaller than b')
-# else:
-#     print('a is equal to b')
+if a > b:
+    print('a is greater than b')
+elif a < b:
+    print('a is smaller than b')
+else:
+    print('a is equal to b')
 
 
 # level 2
 
 # students grading
 
-# scores = int(input('enter your score: '))
+scores = int(input('enter your score: '))
 
-# if scores >= 90:
-#     print('a')
-# elif scores >= 80:
-#     print('b')
-# elif scores >= 70:
-#     print('c')
-# elif scores >= 60:
-#     print('d')
-# elif scores >= 0:
-#     print('f')
-# else:
-#     print('score not found!')
+if scores >= 90:
+    print('a')
+elif scores >= 80:
+    print('b')
+elif scores >= 70:
+    print('c')
+elif scores >= 60:
+    print('d')
+elif scores >= 0:
+    print('f')
+else:
+    print('score not found!')
 
 
 # season checker
-# season_input = input('Enter a month to know what season it is: ').lower()
-# winter_months = ['december','january', 'february']
-# autumn_months = ['september', 'october', 'november']
-# summer_months = ['june', 'july', 'august']
-# spring_months = ['march', 'april', 'may']
+season_input = input('Enter a month to know what season it is: ').lower()
+winter_months = ['december','january', 'february']
+autumn_months = ['september', 'october', 'november']
+summer_months = ['june', 'july', 'august']
+spring_months = ['march', 'april', 'may']
 
-# if season_input in winter_months:
-#     print('Winter')
-# elif season_input in autumn_months:
-#     print('Autumn')
-# elif season_input in summer_months:
-#     print('Summer')
-# elif season_input in spring_months:
-#     print('Spring')
-# else:
-#     print('enter a valid month!')
+if season_input in winter_months:
+    print('Winter')
+elif season_input in autumn_months:
+    print('Autumn')
+elif season_input in summer_months:
+    print('Summer')
+elif season_input in spring_months:
+    print('Spring')
+else:
+    print('enter a valid month!')
 
 
 
-# fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits = ['banana', 'orange', 'mango', 'lemon']
 
-# add_fruit = input('enter a fruit: ').lower()
+add_fruit = input('enter a fruit: ').lower()
 
-# if add_fruit in fruits:
-#     print('That fruit already exist in the list')
-# else:
-#     fruits.append(add_fruit)
-#     print(f'added! updated list {fruits}')
+if add_fruit in fruits:
+    print('That fruit already exist in the list')
+else:
+    fruits.append(add_fruit)
+    print(f'added! updated list {fruits}')
 
 
 # level 3
