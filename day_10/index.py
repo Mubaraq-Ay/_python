1940cba90adb2ae5e232ab62dbc6be88710916f3
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-03 11:41:07 -0700

    while...else

--- day_10/index.py
+++ day_10/index.py
@@ -5,10 +5,20 @@
 # while loop
 # this is used to execute a block of statements repeatedly until a given condition is satisfied.
 
+# count = 0
+
+# while count < 5:
+#     print(count)
+#     count = count + 1
+
+
+# while else
+# A while...else statement is a loop where the else block executes only if the while loop finishes normally (without a break).
+
 count = 0
 
 while count < 5:
     print(count)
-    count = count + 1
-
-    
+    count += 1
+else:
+    print(count)
