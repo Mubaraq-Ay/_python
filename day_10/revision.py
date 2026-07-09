d852352b6c4988912943a02dc6ee6a5b8c06034c
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-08 21:36:10 -0700

    restarted loops again

---
+++ day_10/revision.py
@@ -0,0 +1,29 @@
+# while loop
+
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
+
+# 1
+
+numbers = 1
+
+while numbers <= 20:
+    print(numbers)
+    numbers += 1
+
+# 2
+
+countdown = 15
+
+while countdown >= 1:
+    print(countdown)
+    countdown -= 1
+else:
+    print('Blast off! 🚀')
+
+
+# 3
+
