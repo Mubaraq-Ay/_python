8d9b6bd2886825a5cfee169b0319009596ea05a7
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-16 01:38:30 -0700

    the square problem

--- day_10/exercises.py
+++ day_10/exercises.py
@@ -35,4 +35,52 @@ for triangle in range(7):
 # shorter version
 for triangle in range(8, 1):
     print('#' * triangle)
-    
+
+# 4
+# nested loops.
+
+for row in range(8):
+    for column in range(8):
+     print('#', end=' ')
+    print()
+   
+
+# 5.
+
+for numbers in range(11):
+    print(f'{numbers} x {numbers} = {numbers ** 2}')
+
+# 6.
+
+languages = ['Python', 'Numpy','Pandas','Django', 'Flask']
+
+for language in languages:
+    print(language)
+
+# 7. 
+
+# print only even numbers from 0 -100
+for numbers in range(101):
+    if numbers % 2 == 0:
+        print(numbers)
+
+# 8
+
+# print only odd numbers from 0 - 100
+for numbers in range(101):
+    if numbers % 2 == 1:
+        print(numbers)
+
+
+# level 2
+
+# 1
+
+# sum of 0 - 100, using for loop.
+# for numbers in range(101):
+#     print(numbers)
+
+# 
+
+
+
