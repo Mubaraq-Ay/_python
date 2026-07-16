c06af02586029e06c42051613ddb297a79b4a480
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-16 15:17:19 -0700

    functions without parameters

---
+++ day_11/index.py
@@ -0,0 +1,23 @@
+# functions.
+
+# A function is a reusable block of code that performs a specific task and can be called whenever it is needed.
+# we use def keyword.
+
+# function without parameters.
+# function can be declared without parameters.
+
+def generate_full_name():
+    first_name = 'mubaraq'
+    last_name = 'ayanleke'
+    space = ' '
+    full_name = first_name + space + last_name
+    print(full_name)
+generate_full_name() # calling a function
+
+def add_two_numbers():
+    num_one = 2
+    num_two = 3
+    total = num_one + num_two
+    print(total)
+add_two_numbers()
+
