253fcb8c6c980c8d3aadaed2959505824b9c6fd8
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-17 19:27:33 -0700

    functions

--- day_11/index.py
+++ day_11/index.py
@@ -21,3 +21,58 @@ def add_two_numbers():
     print(total)
 add_two_numbers()
 
+
+# function returning a value - part 1
+
+# function return values using the return statement. 
+# if a function has no return statement, it returns None.
+
+def generate_full_name():
+    first_name = 'mubaraq'
+    last_name = 'ayanleke'
+    space = ' '
+    full_name = first_name + space + last_name
+    return full_name
+print(generate_full_name())
+
+
+# tasks
+
+def say_hello():
+    say_hello = 'hello!'
+    print(say_hello)
+say_hello()
+
+# 2
+
+def school_name():
+    school_name ='oui'
+    print(school_name)
+school_name()
+
+
+# 3
+
+def favorite_language():
+    fav_language = 'python'
+    return fav_language
+print(favorite_language())
+
+# 4
+
+def favorite_number():
+    favorite_number = 1
+    return favorite_number
+print(favorite_number())
+
+# 5
+
+def multiply():
+    nums = 7 * 9
+    return nums
+print(multiply())
+
+
+# 6
+
+ 
