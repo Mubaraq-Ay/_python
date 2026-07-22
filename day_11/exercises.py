9d1b4fc95aedd73789f98acaf3b9892f8874f754
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-22 01:06:09 -0700

    write a function that calculates the area of a circle.

---
+++ day_11/exercises.py
@@ -0,0 +1,11 @@
+def add_two_numbers(a, b):
+    total = a + b
+    return total
+print(add_two_numbers(3, 1))
+
+# write a function that calculates the area of a circle.
+def area_of_circle(pi, r):
+    area = pi * r ** 2
+    return(area)
+print(area_of_circle(3.142, 3))
+
