11e151d62792f3592207cf6fce83accd9572af41
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-22 10:20:32 -0700

    convert degrees to fahrenheit

--- day_11/exercises.py
+++ day_11/exercises.py
@@ -9,3 +9,15 @@ def area_of_circle(pi, r):
     return(area)
 print(area_of_circle(3.142, 3))
 
+def add_all_nums(*nums):
+    for num in nums:
+        if not isinstance(num, (int, float)):
+            return 'All arguments must be numbers..'
+        
+    return sum(nums)
+print(add_all_nums(1,2,4,'ld'))
+
+def convert_celcius_to_fahrenheit(c):
+    conversion = str((c * 9/5) + 32) + ' °F'
+    return conversion
+print(convert_celcius_to_fahrenheit(3))
