89671dfc19b070ced370127a107c6702a02da889
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-23 12:02:55 -0700

    some tasks

--- day_12/tinker/main.py
+++ day_12/tinker/main.py
@@ -1,6 +1,10 @@
 from school import school_name, student, lecturer
+from shapes import pi as PI, area as circle_area
 
 print(student('mubaraq'))
 print(school_name)
 print(lecturer['name'])
 print(lecturer['course'])
+
+print(circle_area(5))
+print(PI)
