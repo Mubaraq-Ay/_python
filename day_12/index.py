1556f47e4ca70b6d6abc4608fdb12194eef6fd6a
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-23 11:05:48 -0700

    modules

---
+++ day_12/index.py
@@ -0,0 +1,12 @@
+# modules.
+# a module is a file containing a set of codes/functions which can be included in an application.
+
+# creating a module.
+# to create a module, we write the code in a python script and save it as a .py file.
+
+
+# importing a module
+# we use the import keyword and the file name only.
+
+import mymodule
+print(mymodule.generate_full_name('mubaraq', 'ayanleke'))
