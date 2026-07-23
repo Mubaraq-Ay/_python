75d0d291ec04ae31dd15b9ca3680ae002734bb4f
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-23 15:36:36 -0700

    mini tasks on os modules

---
+++ day_12/tinker/os_tasks.py
@@ -0,0 +1,23 @@
+import os
+
+print(os.getcwd())
+
+# navigation.
+
+os.chdir(r'C:\Users\Admin\Desktop\python\day_12')
+print(os.getcwd())
+
+os.mkdir('practice')
+print(os.listdir())
+os.rmdir('practice')
+print(os.listdir())
+
+# create directory
+os.mkdir('Backend_project')
+# change to the directory
+os.chdir(r'C:\Users\Admin\Desktop\python\day_12\Backend_project')
+# pwd
+print(os.getcwd())
+
+
+ 
