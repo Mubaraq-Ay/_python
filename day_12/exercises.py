b7427bbc79606c263c6deaf0d5e9178191a9cb87
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-27 14:41:54 -0700

    mini tasks

--- day_12/exercises.py
+++ day_12/exercises.py
@@ -12,3 +12,66 @@ def random_user_id():
        
 print(random_user_id())
 
+# random password.
+
+def random_password():
+     password = string.ascii_letters + string.digits
+     random_pass = ''
+
+     for i in range(8):
+         random_pass += choice(password)
+     return random_pass
+print(random_password())
+
+# just doing this to test smt
+def pass_with_characters():
+     password = string.ascii_letters + string.digits + string.punctuation
+     rand = ''
+
+     for i in range(12):
+        rand += choice(password)
+     return rand
+print(pass_with_characters())
+
+# 2
+def coin_tosses():
+    outcome = 'HT'
+    rs = ''
+
+    for i in range(10):
+        rs += choice(outcome)
+    return rs
+print(coin_tosses())
+
+# generate random plates.
+def random_chars(pool, length):
+    result = ''
+
+    for i in range(length):
+        result += choice(pool)
+
+    return result
+
+def random_plate():
+    letters = random_chars(string.ascii_uppercase, 2)
+    numbers = random_chars(string.digits, 3)
+    last_letters = random_chars(string.ascii_uppercase, 2)
+
+    return f'{letters}-{numbers}-{last_letters}'
+print(random_plate())
+
+# nigerian version
+def rand_char(pool, length):
+    result = ''
+
+    for i in range(length):
+        result += choice(pool)
+    return result
+
+def plates():
+    letters = rand_char(string.ascii_uppercase, 3)
+    numbers = rand_char(string.digits, 3)
+    last_letters = rand_char(string.ascii_uppercase, 2)
+
+    return f'{letters}-{numbers}-{last_letters}'
+print(plates())
