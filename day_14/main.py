8349f38cd4f7791461a00cfab62744ddb603669c
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-28 14:06:00 -0700

    python closures

--- day_14/main.py
+++ day_14/main.py
@@ -58,3 +58,55 @@ result = highest_order_function('absolute')
 print(result(-3))
 
 # python closures.
+# this is an inner function that remembers variables from the outer function, even after the outer function has finished running.
+
+def add_ten():
+    ten  = 10
+    def add(num):
+        return num + ten
+    return add
+
+closure_result = add_ten()
+print(closure_result(5))
+print(closure_result(10))
+
+def add_five():
+    five = 5
+    def add(num):
+        return num + five
+    return add
+
+plus_five = add_five()
+print(plus_five(10))
+print(plus_five(30))
+print(plus_five(-2))
+
+
+def multiply_by(number):
+    def multiply(num):
+        return number * num
+    return multiply
+
+double = multiply_by(2)
+triple = multiply_by(3)
+
+print(double(2))
+print(triple(2))
+
+def say_hello(name):
+    def greeting():
+        return f'hello {name}'
+    return greeting
+
+greet_mubaraq = say_hello('mubaraq')
+greet_amad = say_hello('amad')
+
+print(greet_mubaraq())
+
+def add_number(number):
+    def numb(num):
+        return number + num
+    return numb
+
+add_five = add_number(5)
+print(add_five(3))
