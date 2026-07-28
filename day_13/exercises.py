79fadc266202a098b43ec5b694b0c288ae31e53e
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-07-28 00:42:00 -0700

    tuples

--- day_13/exercises.py
+++ day_13/exercises.py
@@ -7,3 +7,7 @@ print(negative_and_zero)
 list_of_lists =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 flattened_list = [number for row in list_of_lists for number in row]
 print(flattened_list)
+
+# tuples
+numbers = [(i, 1, i, i ** 2, i ** 3, i ** 4, i ** 5) for i in range(11)]
+print(numbers)
