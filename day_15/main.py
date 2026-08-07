7efabf4ab4d6fc268efc62599e891e95ea53e8e7
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-06 17:00:52 -0700

    NameError & indexerror

--- day_15/main.py
+++ day_15/main.py
@@ -1,6 +1,30 @@
 # python type errors.
 
-# syntax error.
-# print 'hello world' 
+# SyntaxError.
 
+# print 'hello world' - wrong syntax, because of the parenthesis
+
+# correct way.
 print('hello world')
+
+
+# NameError.
+
+# print(age) - we can't do this because age is not defined yet and we would get a name error if we print this
+
+# correct way
+age = 10
+print(age)
+
+
+# IndexError.
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers[5]      wont work because the index is out of range. there are only 4 indexes in this list btw, so it's 0 to 4.
+
+
+# ModuleNotFoundError
+
+# import maths  - no module named maths - wrong module, the correct module is math.
+
+# correct way
