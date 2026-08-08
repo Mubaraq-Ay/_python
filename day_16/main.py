f0139dfb80478263d55bb179a2b3496787c41a08
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-07 17:44:33 -0700

    formatting date output using strftime

--- day_16/main.py
+++ day_16/main.py
@@ -24,15 +24,57 @@ print(year)
 hour = now.hour
 print(hour)
 
-min = now.minute
-print(min)
+minute = now.minute
+print(minute)
 
-sec = now.second
-print(sec)
+seconds = now.second
+print(seconds)
 
 timestamp = now.timestamp()
 print(timestamp) 
 
 # timestamp or unix timestamp is the number of seconds elapsed since jan 1, 1970.
 
-print(f'{day}/{month}/{year}, {hour}:{min}')
+print(f'{day}/{month}/{year}, {hour}:{minute}')
+
+
+# formatting date output using strftime.
+from datetime import datetime
+
+new_year = datetime(2020, 1, 1)
+print(new_year)
+
+day = new_year.day
+print(day)
+
+month = new_year.month
+print(month)
+
+year = new_year.year
+print(year)
+
+hour = new_year.hour
+print(hour)
+
+minute = new_year.minute
+print(minute)
+
+seconds = new_year.second
+print(seconds)
+
+print(f'{day}/{month}/{year}, {hour}:{minute}')
+
+
+# using strftime now.
+from datetime import datetime
+
+# current date and time
+now = datetime.now()
+t = now.strftime("%H:%M:%S")
+print('time:', t)
+
+time_one = now.strftime('%m/%d/%Y, %H:%M:%S')
+print(f'time_one: {time_one}')
+
+time_two = now.strftime('%d/%m/%Y, %H:%M:%S')
+print(f'time_two: {time_two}')
