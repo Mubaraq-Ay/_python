71165c56ed718f51b28026c043e077e8a7e2f77a
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-12 08:30:38 -0700

    spreading in python

--- day_17/main.py
+++ day_17/main.py
@@ -285,3 +285,14 @@ print(packing_person_info(
       age=250
 ))
 
+
+# spreading in python
+
+lst_one = [1, 2, 3]
+lst_two = [4, 5, 6, 7]
+lst = [0, *lst_one, *lst_two]
+print(lst)
+country_lst_one = ['Finland', 'Sweden', 'Norway']
+country_lst_two = ['Denmark', 'Iceland']
+nordic_countries = [*country_lst_one, *country_lst_two]
+print(nordic_countries)
