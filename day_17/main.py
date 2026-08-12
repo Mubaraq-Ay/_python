5e8b62e60221800f0ecbb1fc913748afede06048
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-12 08:23:18 -0700

    packing lists

--- day_17/main.py
+++ day_17/main.py
@@ -258,3 +258,14 @@ dct = {
 }
 
 print(unpacking_person_info(**dct))
+
+# packing lists
+
+def sum_all(*args):
+    s = 0
+    for i in args:
+        s += i
+    return s
+print(sum_all(1,2,3)) 
+print(sum_all(1,2,3,4,5,6,6,7))
+
