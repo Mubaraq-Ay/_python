2566d7537ee83e8dec1e0371264b31f5d9b3628c
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-12 08:54:24 -0700

    unpacking task.

---
+++ day_17/exercises.py
@@ -0,0 +1,7 @@
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+fin, swe, nor, den, ice, es, ru = names
+
+*nordic_countries, es, ru = names
+print(nordic_countries)
+print(es)
+print(ru)
