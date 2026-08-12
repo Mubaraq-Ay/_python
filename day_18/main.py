1eefa5a7af51720d0c18487ca88b5dc6ff935fb8
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-12 09:44:58 -0700

    regular expressions (RegEx), the re module.

---
+++ day_18/main.py
@@ -0,0 +1,9 @@
+# Regular Expressions - RegEx
+# regex is a pattern used to search for, match or validate specific text patterns in data.
+# to use regex in python, we import the regex module which is re.
+
+import re
+# after importing the module, we can use it to detect or find patterns.
+
+# methods in re module.
+
