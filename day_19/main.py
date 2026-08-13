705e49773d6de7dec8ce6384708bb1a9265c9ac1
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-13 13:46:59 -0700

    initial commit: file handling

---
+++ day_19/main.py
@@ -0,0 +1,9 @@
+# file handling.
+
+# file handling is an important part of programming that allows us to 
+# create, read, update and delete files.
+# We use the built-in open() function to work with files.
+
+# syntax
+# open('filename', mode)
+
