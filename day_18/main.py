64ec320cbeab4f0c2060f980472762864a7fb24e
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-13 08:48:35 -0700

    replace a substring using re.sub

--- day_18/main.py
+++ day_18/main.py
@@ -164,3 +164,25 @@ print(matches)
 
 matches = re.findall('[Pp]ython', txt)
 print(matches)
+
+
+# replace a substring
+
+txt = '''Python is the most beautiful language that a human being has ever created.
+I recommend python for a first programming language'''
+
+match_replaced = re.sub('Python|python', 'Javascript', txt, flags=re.I)
+print(match_replaced)
+
+# or
+match_replaced = re.sub('[Pp]ython', 'Javascript', txt, flags=re.I)
+print(match_replaced)
+
+
+txt = '''%I a%m te%%a%%che%r% a%n%d %% I l%o%ve te%ach%ing.
+T%he%re i%s n%o%th%ing as r%ewarding a%s e%duc%at%i%ng a%n%d e%m%p%ow%er%ing p%e%o%ple.
+I fo%und te%a%ching m%ore i%n%t%er%%es%ting t%h%an any other %jobs.
+D%o%es thi%s m%ot%iv%a%te %y%o%u to b%e a t%e%a%cher?'''
+
+matches = re.sub('%', '', txt)
+print(matches)
