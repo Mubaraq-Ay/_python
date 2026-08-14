ed92fa64a54914da839e68bd833dd8460040a46f
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-14 01:28:02 -0700

    file with json extension

--- day_19/main.py
+++ day_19/main.py
@@ -100,3 +100,24 @@ else:
 # file with txt extension.
 # this is a common form of data. 
 
+# file with json extension
+# json - javascript object notation.  
+# it is a string form of dictionary.
+
+# dictionary
+person_dct= {
+    "name":"Asabeneh",
+    "country":"Finland",
+    "city":"Helsinki",
+    "skills":["JavaScrip", "React","Python"]
+}
+# JSON: A string form a dictionary
+person_json = "{'name': 'Asabeneh', 'country': 'Finland', 'city': 'Helsinki', 'skills': ['JavaScrip', 'React', 'Python']}"
+
+# we use three quotes and make it multiple line to make it more readable
+person_json = '''{
+    "name":"Asabeneh",
+    "country":"Finland",
+    "city":"Helsinki",
+    "skills":["JavaScrip", "React","Python"]
+}'''
