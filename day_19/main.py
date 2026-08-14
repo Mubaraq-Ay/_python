ff5b1dfe5611919fbc367a1cca4dede96cb8f2e2
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-14 01:32:46 -0700

    changing json to dictionary

--- day_19/main.py
+++ day_19/main.py
@@ -21,78 +21,78 @@
 # opening files for reading.
 # the default mode for opening a file is reading. we dont need to specify 'r' or 'rt'
 
-f = open('../reading_file_example.txt')
-print(f) 
+# f = open('../reading_file_example.txt')
+# print(f) 
 
-txt = f.read()
-print(type(txt))
-print(txt)
-f.close()
+# txt = f.read()
+# print(type(txt))
+# print(txt)
+# f.close()
 
-# instead of printing all the text, print the furst 10 characters of the text file.
+# # instead of printing all the text, print the furst 10 characters of the text file.
 
-f = open('../reading_file_example.txt')
-txt = f.read(10)
-print(type(txt))
-print(txt)
-
-
-# readline() - prints the first line of the file.
-
-f = open('../reading_file_example.txt')
-line = f.readline()
-print(type(line))
-print(line)
-
-# readlines -  read all the text line by line and returns a list of lines.
-
-f = open('../reading_file_example.txt')
-lines = f.readlines()
-print(type(lines))
-print(lines)
-f.close()
-
-# another way to get all the lines as a list is using splitlines()
-
-f = open('../reading_file_example.txt')
-lines = f.read().splitlines()
-print(type(lines))
-print(lines)
-f.close()
-
-# new way of closing a file.
-
-with open('../reading_file_example.txt') as f:
-    lines = f.read().splitlines()
-    print(type(lines))
-    print(lines)
+# f = open('../reading_file_example.txt')
+# txt = f.read(10)
+# print(type(txt))
+# print(txt)
 
 
-# opening files for writing and updating
-# to write an existing file, we must add mode as a parameter to the open() function.
+# # readline() - prints the first line of the file.
 
-'''
-    a - append - will append to the end of the file. if file does not exist it creates a new file.
-    w - write - will overwrite any existing content, if the file does not exist it creates.
-'''
+# f = open('../reading_file_example.txt')
+# line = f.readline()
+# print(type(line))
+# print(line)
 
-with open('../reading_file_example.txt', 'a') as f:
-    f.write('this is the appended text')
+# # readlines -  read all the text line by line and returns a list of lines.
 
-# this method creates a new file if the file does not exist
-with open('../reading_file_example.txt', 'w') as f:
-    f.write('this is a newly created file') # if file exist sef, it clears everything there and upload what is here.
+# f = open('../reading_file_example.txt')
+# lines = f.readlines()
+# print(type(lines))
+# print(lines)
+# f.close()
+
+# # another way to get all the lines as a list is using splitlines()
+
+# f = open('../reading_file_example.txt')
+# lines = f.read().splitlines()
+# print(type(lines))
+# print(lines)
+# f.close()
+
+# # new way of closing a file.
+
+# with open('../reading_file_example.txt') as f:
+#     lines = f.read().splitlines()
+#     print(type(lines))
+#     print(lines)
 
 
-# deleting files
-# we use os module if we want to remove a file ;)
+# # opening files for writing and updating
+# # to write an existing file, we must add mode as a parameter to the open() function.
 
-import os
+# '''
+#     a - append - will append to the end of the file. if file does not exist it creates a new file.
+#     w - write - will overwrite any existing content, if the file does not exist it creates.
+# '''
 
-if os.path.exists('../reading_file_example.txt'):
-    os.remove('../reading_file_example.txt')
-else:
-    print('this file does not exist.')
+# with open('../reading_file_example.txt', 'a') as f:
+#     f.write('this is the appended text')
+
+# # this method creates a new file if the file does not exist
+# with open('../reading_file_example.txt', 'w') as f:
+#     f.write('this is a newly created file') # if file exist sef, it clears everything there and upload what is here.
+
+
+# # deleting files
+# # we use os module if we want to remove a file ;)
+
+# import os
+
+# if os.path.exists('../reading_file_example.txt'):
+#     os.remove('../reading_file_example.txt')
+# else:
+#     print('this file does not exist.')
 
 
 # file types
@@ -121,3 +121,21 @@ person_json = '''{
     "city":"Helsinki",
     "skills":["JavaScrip", "React","Python"]
 }'''
+
+
+# changing json to dictionary
+
+import json
+
+person_json = '''{
+    "name": "mubaraq",
+    "country": "qatar",
+    "city": "abj",
+    "skills": ["javacript", "python", "docker"]
+}'''
+
+# changing the json to dictionary
+person_dct = json.loads(person_json)
+print(type(person_dct))
+print(person_dct)
+print(person_dct['name'])
