eb233c06c8ddcdddd032b3ed1094cd022412761c
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-16 00:20:49 -0700

    initial commit

---
+++ day_20/test.py
@@ -0,0 +1,7 @@
+from mypackage import arithmetic, greet
+
+print(arithmetic.add_numbers(1,2,5))
+print(arithmetic.subtract(2,1))
+print(arithmetic.division(4, 2))
+
+print(greet.greet_person('mubaraq', 'ayanleke'))
