519e39fa2a4d2c6e391ff6bf3e27917d02d7191e
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-15 19:56:41 -0700

    pip

---
+++ day_20/main.py
@@ -0,0 +1,12 @@
+# Python pip - Python package manager.
+# pip - Preferred Installer Program. We use pip to install Python packages.
+# A package is a collection of Python modules and can contain other packages.
+# We don't need to write every utility ourselves; we can install packages
+# and import them into our applications.
+
+# installing pip.
+
+# we install using -: pip install pip
+
+# check pip version -: pip --version
+
