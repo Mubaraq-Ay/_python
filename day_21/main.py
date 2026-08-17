8d014847849c7dd691472439a67bc2446d077134
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-17 01:04:44 -0700

    class constructor

--- day_21/main.py
+++ day_21/main.py
@@ -9,7 +9,7 @@
 
 
 # creating a class
-# class name should be in CamelCase.
+# # class names conventionally use PascalCase (CamelCase).
 # syntax:= 
 # class ClassName:
 #   code...
@@ -23,3 +23,56 @@ print(Person)
 
 p = Person()
 print(p) 
+
+
+# class constructor.
+
+# without the constructor, the class is basically useless. you have to manually set every attribute.
+
+class Person:
+    pass
+
+p1 = Person()
+p1.name = "Mubaraq"   # you'd have to manually set every attribute, every time
+p1.age = 20
+
+# but with the constructor __init__
+
+class Person:
+    def __init__(self, name, age):
+        # self allows to attach parameter to the class.
+        self.name = name
+        self.age = age
+
+p1 = Person("mubaraq", 20)
+p2 = Person("ali", 21)
+
+print(p1.name, p1.age)
+print(p2.name, p2.age)
+
+class Me:
+    def __init__(self, firstname, lastname, age, country, city):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.country = country
+        self.city = city
+
+p = Me('mubaraq', 'ayanleke', 250, 'kuwaiti', 'dinar')
+print(p.firstname, p.lastname, p.age, p.country, p.city)
+
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def describe(self):
+        print(f'this is a {self.make} {self.model} {self.year}')
+
+corolla = Car('toyota', 'corolla', 2020)
+corolla.describe()
+
+accord = Car('honda', 'accord', 2020)
+accord.describe()
