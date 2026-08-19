6882f2c803af65e48c4f7378586382614f4e4612
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-19 10:48:58 -0700

    venv

--- day_23/main.py
+++ day_23/main.py
@@ -1,1 +1,13 @@
 # virtual environments
+# a virtual environment is an isolated python environment for one project with its own installed packages.
+
+# it avoids conflicting of differnt projects.
+
+# core commands
+
+'''
+    python -m venv venv        # creates a virtual environment named "venv" in your project folder
+    source venv/bin/activate   # activates it (Linux/Fedora — you'll use this one)
+
+    deactivate    -> leaves the virtual environment
+'''
