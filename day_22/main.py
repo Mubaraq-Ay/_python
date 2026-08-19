1c84f80a5574b7823493ddbb43fe3810dc05eccf
Mubaraq Ayanleke <ayanlekemubaraq@gmail.com>
2026-08-19 10:21:36 -0700

    short stuff

--- day_22/main.py
+++ day_22/main.py
@@ -1,1 +1,5 @@
 # web scraping
+# this is pulling data off websites automatically, by writing code that downloads a webpage html 
+# and extract specific pieces of it.
+
+# we use requests, beautifulsoup 
